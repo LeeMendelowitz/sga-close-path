9930d5122eb362d8ae828a2b6d036334858cca51
Lee Mendelowitz <LMendy@gmail.com>
2012-12-24 22:47:15 -0500

    Added Allocator.h

---
+++ src/Util/Allocator.h
@@ -0,0 +1,29 @@
+//-----------------------------------------------
+// Copyright 2010 Wellcome Trust Sanger Institute
+// Written by Jared Simpson (js18@sanger.ac.uk)
+// Released under the GPL license
+//-----------------------------------------------
+//
+// Allocator interface
+
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+
+class Allocator
+{
+    public:
+        Allocator() {}
+        virtual ~Allocator() {}
+
+        // Allocate some memory
+        virtual void* alloc() = 0;
+
+        // Deallocate memory
+        virtual void dealloc(void *) = 0;
+
+        // Reset the allocator
+        virtual void reset() = 0;
+};
+
+#endif
